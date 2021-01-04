61d572309d512a40ed908ac5abc3bb9209c1ac67
Haosong Li <hdlee9885@hotmail.com>
2021-01-03 23:50:43 -0600

    Added theme transitions

---
+++ theme.js
@@ -0,0 +1,37 @@
+let themeDots = document.getElementsByClassName('theme-dot');
+
+let currTheme = localStorage.getItem('theme');
+
+if (currTheme == null) {
+    setTheme('light');
+} else {
+    setTheme(currTheme);
+}
+
+for (let i = 0; i < themeDots.length; i++) {
+    themeDots[i].addEventListener('click', () => {
+        let mode = themeDots[i].dataset.mode;
+        console.log(mode)
+        setTheme(mode);
+    })
+}
+
+function setTheme(mode) {
+    if (mode == 'light') {
+        document.getElementById('theme-style').href = 'portfolio.css';
+    }
+
+    if (mode == 'blue') {
+        document.getElementById('theme-style').href = 'blue.css';
+    }
+
+    if (mode == 'green') {
+        document.getElementById('theme-style').href = 'green.css';
+    }
+
+    if (mode == 'purple') {
+        document.getElementById('theme-style').href = 'purple.css';
+    }
+
+    localStorage.setItem('theme', mode);
+}
