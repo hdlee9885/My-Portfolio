9cc5ee58de3aa51028f1bb2c22921dfe69a811e0
Haosong Li <hdlee9885@hotmail.com>
2021-01-08 00:08:59 -0600

    Added section animation

--- public/javascript/theme.js
+++ public/javascript/theme.js
@@ -53,3 +53,79 @@ $(document).ready(function() {
         }
     });
 });
+
+// let mouseCursor = document.querySelector(".cursor");
+// window.addEventListener("mousemove", (e)=> {
+//     mouseCursor.style.top = e.pageY + 'px';
+//     mouseCursor.style.left = e.pageX + 'px';
+// })
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin, dest, direction) => {
+        const section = dest.item;
+        const leftColumn = section.querySelector(".left-column");        const rightCol = section.querySelector(".right-column");
+        const tl = new TimelineMax({ delay: 0.5 });
+        if (dest.index === 0) {
+            // leftColumn = document.querySelector(".left-column");
+            // rightCol = document.querySelector(".right-column");
+            // tl.fromTo(leftColumn, 1, { x: '-200', opacity: 0 }, { x: '0', opacity: 1 })
+            //   .fromTo(rightCol, 1, { x: '200', opacity: 0 }, { x: '0', opacity: 1 });
+        }
+
+        if (dest.index === 1) {
+            const aboutMe = document.querySelector(".about-me");
+            const ztech = document.querySelector(".work");
+            const education = document.querySelector(".edu-wrapper");
+            const cmu = document.querySelector("#cmu");
+            const uta = document.querySelector('#uta');
+            const iit = document.querySelector('#iit');
+            tl.fromTo(aboutMe, 1, { x: '-200', opacity: 0 }, { x: '0', opacity: 1 })
+              .fromTo(ztech, 0.7, { opacity: 0 }, { opacity: 1 })
+              .fromTo(education, 1, { x: '200', opacity: 0 }, { x: '0', opacity: 1 }, '-=1')
+              .fromTo(cmu, 0.7, { opacity: 0 }, { opacity: 1 })
+              .fromTo(uta, 0.7, { opacity: 0 }, { opacity: 1 })
+              .fromTo(iit, 0.7, { opacity: 0 }, { opacity: 1 });
+        }
+
+        if (dest.index === 2) {
+            const projects = document.querySelectorAll(".project");
+            const projectTitle = document.querySelector("#project-title");
+            tl.fromTo(projectTitle, 1, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 });
+            for (let i = 0; i < projects.length; i++) {
+                tl.fromTo(projects[i], 1, { y: '200', opacity: 0 }, { y: '0', opacity: 1 });                
+            }
+        }
+
+        if (dest.index === 3) {
+            const contactTitle = document.querySelector("#contact-title");
+            const form = document.querySelector(".form-wrapper");
+            tl.fromTo(contactTitle, 1, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 })
+              .fromTo(form, 1, { y: '200', opacity: 0 }, { y: '0', opacity: 1 }, '-=1');
+        }
+    }
+    // onLeave: (origin, dest, direction) => {
+    //     const section = dest.item;
+    //     const title = section.querySelector('h1');
+    //     const tl = new TimelineMax({delay: 0.5});
+    //     tl.fromTo(title, 0.5, {y: '50', opacity: 0}, {y: '0', opacity: 1});
+
+    //     if (dest.index === 1) {
+    //         const chairs = document.querySelectorAll(".chair");
+    //         const description = document.querySelector(".description");
+    //         tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-50%" })
+    //           .fromTo(
+    //             description,
+    //             0.5,
+    //             { opacity: 0, y: "50" },
+    //             { y: "0", opacity: 1 }
+    //           )
+    //           .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
+    //           .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
+    //           .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+    //       }
+    // }
+
+
+})
