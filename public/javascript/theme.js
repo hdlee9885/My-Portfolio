bb94f7e2de65a3599e8bb5569414e031587b210c
Haosong Li <hdlee9885@hotmail.com>
2021-01-08 17:24:06 -0600

    Added card animation

--- public/javascript/theme.js
+++ public/javascript/theme.js
@@ -54,6 +54,49 @@ $(document).ready(function() {
     });
 });
 
+// 3d card effect
+
+const container = document.querySelectorAll(".project-container");
+const project = document.querySelectorAll(".project");
+
+const projectImg = document.querySelectorAll(".thumbnail");
+const projectTitle = document.querySelectorAll(".project-title");
+const projectIntro = document.querySelectorAll(".project-intro");
+const projectButton = document.querySelectorAll(".project-link");
+
+for (let i = 0; i < container.length; i++) {
+    container[i].addEventListener('mousemove', (e) => {
+        console.log(window.innerWidth, innerHeight);
+        console.log(e.pageX, e.pageY);
+        let x = ((window.innerWidth - e.pageX) / (container.length - i)) / ((container.length - i) * 300);
+        let y = ((window.innerHeight - e.pageY) / (container.length - i)) / ((container.length - i) * 300);
+        project[i].style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    });
+    
+    container[i].addEventListener('mouseenter', (e) => {
+        project[i].style.transition = "none";
+        // pop out
+        projectTitle[i].style.transform = 'translateZ(150px)';
+        projectImg[i].style.transform = 'translateZ(125px)';
+        // $(".thumbnail").css({"border-radius": "100px"});
+        projectIntro[i].style.transform = "translateZ(150px)";
+        projectButton[i].style.transform = "translateZ(150px)";
+    });
+    
+    container[i].addEventListener('mouseleave', (e) => {
+        project[i].style.transition = "all 0.75s ease";
+        project[i].style.transform = `rotateY(0deg) rotateX(0deg)`;
+        // pop out
+        projectTitle[i].style.transform = 'translateZ(0px)';
+        // $(".thumbnail").css({"border-radius": "0px"});
+        projectImg[i].style.transform = 'translateZ(0px)';
+        projectIntro[i].style.transform = "translateZ(0px)";
+        projectButton[i].style.transform = "translateZ(0px)";
+    });
+}
+
+
+
 // let mouseCursor = document.querySelector(".cursor");
 // window.addEventListener("mousemove", (e)=> {
 //     mouseCursor.style.top = e.pageY + 'px';
@@ -66,7 +109,7 @@ new fullpage('#fullpage', {
     onLeave: (origin, dest, direction) => {
         const section = dest.item;
         const leftColumn = section.querySelector(".left-column");        const rightCol = section.querySelector(".right-column");
-        const tl = new TimelineMax({ delay: 0.5 });
+        const tl = gsap.timeline({ defaults: { ease: 'power1.oot' } });
         if (dest.index === 0) {
             // leftColumn = document.querySelector(".left-column");
             // rightCol = document.querySelector(".right-column");
@@ -83,31 +126,31 @@ new fullpage('#fullpage', {
             const cmu = document.querySelector("#cmu");
             const uta = document.querySelector('#uta');
             const iit = document.querySelector('#iit');
-            tl.fromTo(aboutMe, 1, { x: '-200', opacity: 0 }, { x: '0', opacity: 1 })
-              .fromTo(ztech, 0.7, { opacity: 0 }, { opacity: 1 })
-              .fromTo(education, 1, { x: '200', opacity: 0 }, { x: '0', opacity: 1 }, '-=1')
-              .fromTo(cmu, 0.7, { opacity: 0 }, { opacity: 1 })
-              .fromTo(uta, 0.7, { opacity: 0 }, { opacity: 1 })
-              .fromTo(iit, 0.7, { opacity: 0 }, { opacity: 1 })
-              .fromTo(expertise, 1, { x: '-200', opacity: 0 }, { x: '0', opacity: 1 })
-              .fromTo(skills, 1, { opacity: 0 }, { opacity: 1 });
+            tl.fromTo(aboutMe, 0.5, { x: '-200', opacity: 0 }, { x: '0', opacity: 1, delay: 0.5 })
+              .fromTo(ztech, 0.5, { opacity: 0 }, { opacity: 1 })
+              .fromTo(education, 0.5, { x: '200', opacity: 0 }, { x: '0', opacity: 1 })
+              .fromTo(cmu, 0.5, { opacity: 0 }, { opacity: 1 })
+              .fromTo(uta, 0.5, { opacity: 0 }, { opacity: 1 })
+              .fromTo(iit, 0.5, { opacity: 0 }, { opacity: 1 })
+              .fromTo(expertise, 0.5, { x: '-200', opacity: 0 }, { x: '0', opacity: 1 })
+              .fromTo(skills, 0.5, { opacity: 0 }, { opacity: 1 });
         }
 
-        if (dest.index === 2) {
-            const projects = document.querySelectorAll(".project");
-            const projectTitle = document.querySelector("#project-title");
-            tl.fromTo(projectTitle, 1, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 });
-            for (let i = 0; i < projects.length; i++) {
-                tl.fromTo(projects[i], 1, { y: '200', opacity: 0 }, { y: '0', opacity: 1 });                
-            }
-        }
+        // if (dest.index === 2) {
+        //     const projects = document.querySelectorAll(".project");
+        //     const projectTitle = document.querySelector("#project-title");
+        //     tl.fromTo(projectTitle, 0.5, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 });
+        //     for (let i = 0; i < projects.length; i++) {
+        //         tl.fromTo(projects[i], 0.5, { y: '200', opacity: 0 }, { y: '0', opacity: 1 });                
+        //     }
+        // }
 
         if (dest.index === 3) {
             const contactTitle = document.querySelector("#contact-title");
             const form = document.querySelector(".form-wrapper");
-            tl.fromTo(contactTitle, 1, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 })
-              .fromTo(form, 1, { y: '200', opacity: 0 }, { y: '0', opacity: 1 }, '-=1');
+            tl.fromTo(contactTitle, 0.5, { y: '-200', opacity: 0 }, { y: '0', opacity: 1 })
+              .fromTo(form, 0.5, { y: '200', opacity: 0 }, { y: '0', opacity: 1 }, '-=1');
         }
     }
 
-})
+});
